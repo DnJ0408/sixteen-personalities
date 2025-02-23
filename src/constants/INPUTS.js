9a451b8351d5f77d7583823a157528bc1b2bd5a9
DnJ0408 <oyj921205@gmail.com>
2025-02-23 18:42:51 +0900

    🎉 Add: Constant Inputs

---
+++ src/constants/INPUTS.js
@@ -0,0 +1,10 @@
+export const LOGIN_INPUTS = [
+  { name: "username", type: "text", placeholder: "id" },
+  { name: "password", type: "password", placeholder: "password" }
+]
+
+export const SIGNUP_INPUTS = [
+  { name: "username", type: "text", placeholder: "id" },
+  { name: "password", type: "password", placeholder: "password" },
+  { name: "nickname", type: "text", placeholder: "nickname" }
+]
