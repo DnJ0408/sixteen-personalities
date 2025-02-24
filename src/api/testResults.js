8120a0d4dcc01eb6802837aa0a304dd095a0c054
DnJ0408 <oyj921205@gmail.com>
2025-02-24 21:05:41 +0900

    ✨ Update : CRUD API for json-server

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -1,8 +1,26 @@
+import axios from 'axios';
+const domain = "http://localhost:5000";
+const resource = "testResults"
+const API_URL = `${domain}/${resource}`
 
-const testResults = () => {
-  return (
-    <div>testResults</div>
-  )
-}
+export const getTestResults = async () => {
+  // 테스트 결과를 요청하고 response에 담는다.
+  const response = await axios.get(API_URL);
+  console.log(response);
+  return response.data;
+};
 
-export default testResults
+export const createTestResult = async (resultData) => {
+  const response = await axios.post(API_URL, resultData);
+  return response.data;
+};
+
+export const deleteTestResult = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  return response.data;
+};
+
+export const updateTestResultVisibility = async (id, visibility) => {
+  const response = await axios.patch(`${API_URL}/${id}`, visibility)
+  return response.data;
+};
