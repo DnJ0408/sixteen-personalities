a895b13a9fc9f635c98fd298d8d18ef05d5b84ff
DnJ0408 <oyj921205@gmail.com>
2025-02-24 19:15:55 +0900

    ✨ Update: Protected Router

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,25 +1,44 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import Login from "../pages/Login"
 import Profile from "../pages/Profile"
 import Signup from "../pages/Signup"
 import Test from "../pages/Test"
 import Results from "../pages/Results"
-
+import Layout from "../components/Layout"
+import useAuthStore from "../zustand/authStore"
 
 const Router = () => {
+
+  const { user } = useAuthStore((state) => state);
+
+  const GuestRoute = () => {
+    return user ? <Navigate to="/" /> : <Outlet />;
+  }
+
+  const PrivateRoute = () => {
+    return user ? <Outlet /> : <Navigate to="/" />
+  }
+
   return (
     <BrowserRouter>
+      <Layout />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/results" element={<Results />} />
+
+        <Route element={<GuestRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/results" element={<Results />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
-
   )
 }
 
