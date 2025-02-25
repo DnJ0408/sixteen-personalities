627ac319a7771ff7bd45204451467be1458b3a60
DnJ0408 <oyj921205@gmail.com>
2025-02-25 15:52:35 +0900

    ✨ Update : added signup function

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,34 @@
 import AuthForm from "../components/AuthForm"
 import { SIGNUP_INPUTS } from "../constants/INPUTS"
+import { register } from "../api/auth"
+import { useState } from "react";
 
 const Signup = () => {
+
+  const [formData, setFormData] = useState({
+    userId: "",
+    password: "",
+    nickname: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    register(formData);
+  }
+
   return (
     <div>
-      <AuthForm title="Signup" inputs={SIGNUP_INPUTS} />
+      <AuthForm
+        title="Signup"
+        inputs={SIGNUP_INPUTS}
+        formData={formData}
+        handleSubmit={handleSubmit}
+
+      />
     </div>
   )
 }
