39af7357d13f95325d8a01b0f73c3bce14bfe54f
DnJ0408 <oyj921205@gmail.com>
2025-02-25 15:51:41 +0900

    ✨ Update: added onSubmit event

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,14 +1,15 @@
 // Login, signup 페이지에 사용
 import { Link } from "react-router-dom"
 
-const AuthForm = ({ title, inputs }) => {
+const AuthForm = ({ title, inputs, formData, handleSubmit }) => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-200 ">
       <div className="max-w-sm w-full p-6 bg-white shadow-lg rounded-xl">
         <h2 className="text-3xl font-bold text-center mb-8">{title}</h2>
-
         <div className="bg-red-50 p-6 rounded-lg">
-          <form className="flex flex-col space-y-6">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col space-y-6">
             {inputs.map((input) => (
               <input
                 key={input.name}
@@ -17,7 +18,9 @@ const AuthForm = ({ title, inputs }) => {
                 className="border border-gray-300 p-3 h-12 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-300"
               />
             ))}
-            <button className="bg-red-600 text-white py-3 h-12 rounded-lg hover:bg-red-700 transition">
+            <button
+              type="submit"
+              className="bg-red-600 text-white py-3 h-12 rounded-lg hover:bg-red-700 transition">
               {title}
             </button>
           </form>
